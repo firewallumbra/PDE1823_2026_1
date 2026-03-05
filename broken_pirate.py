1b0be3c83b74b787f58a2030325f7a3a660a65cf
firewallumbra <169149856+firewallumbra@users.noreply.github.com>
2026-03-05 15:39:45 +0400

    Fixed broken_pirate.py
    Fixes implemented by M01089566

--- broken_pirate.py
+++ broken_pirate.py
@@ -1,5 +1,5 @@
-greeting = input("Hello, possible pirate! What's the password?)
+greeting = input("Hello, possible pirate! What's the password? ") # String was not structured properly due to the absence of a double quotation mark in the end. Fixed by finishing the string with a double quotation mark. Also added a space after the question mark in the string so the input provided by the user is not attached to the question.
 if greeting in ["Arrr!"]: # NG replaced ) by ]
 	print("Go away, pirate.")
-elif
-print("Greetings, hater of pirates!")
+else: # "elif" condition should only be used if an additional alternative condition exists for fulfillment in the case the initial "if" condition is not satisfied. If only one alternative condition is there, else should be used. Fixed by replacing "elif" with "else" and indenting the next line.
+	print("Greetings, hater of pirates!")
